944be09726378d889cd1d6a1a354b5087bd8fb50
kor-coeo <kor.coeo@gmail.com>
2019-07-31 15:49:25 +0800

    init project

---
+++ src/main/java/coeo/ibuki/IbukiApplication.java
@@ -0,0 +1,13 @@
+package coeo.ibuki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbukiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbukiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/coeo/ibuki/IbukiApplicationTests.java
@@ -0,0 +1,16 @@
+package coeo.ibuki;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IbukiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
