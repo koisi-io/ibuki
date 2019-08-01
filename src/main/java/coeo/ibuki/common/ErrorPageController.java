865e15071504ba3fb0f95b73102e33a875a7ad59
kor-coeo <kor.coeo@gmail.com>
2019-08-01 13:43:50 +0800

    fix ErrorPageController @Controller

--- src/main/java/coeo/ibuki/common/ErrorPageController.java
+++ src/main/java/coeo/ibuki/common/ErrorPageController.java
@@ -9,12 +9,14 @@ import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.ModelAndView;
 
+@Controller
 public class ErrorPageController implements ErrorController {
 
     private static ErrorPageController errorPageController;
